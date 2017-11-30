5e52c69c724ce2449b4b8f5ef82514be0167115e
Sspecially <2417744768@qq.com>
2017-11-30 16:28:48 +0800

    add input error color

--- src/views/CommentMain.js
+++ src/views/CommentMain.js
@@ -11,11 +11,16 @@ class CommentMain extends Component {
       times: '8:30-17:30',
       workreason: '',
       remark: '',
-      usernameError: '',
-      departmentError: '',
-      dateError: '',
-      timeError: '',
-      reasonError: ''
+      usernameErrorMsg: '',
+      departmentErrorMsg: '',
+      dateErrorMsg: '',
+      timeErrorMsg: '',
+      reasonErrorMsg: '',
+      usernameErrorColor: 'comment-main-input',
+      departmentErrorColor: 'comment-main-input',
+      dateErrorColor: 'comment-main-input',
+      timeErrorColor: 'comment-main-input',
+      reasonErrorColor: 'comment-main-input'
     }
   }
   handleUsernameChange (event) {
@@ -24,12 +29,15 @@ class CommentMain extends Component {
     })
     if(this.state.username) {
       this.setState({
-        usernameError: ''
+        usernameErrorMsg: ''
       })
     }
   }
   handleUsernameBlur () {
-    this.setState({ usernameError: !this.state.username?'请填写姓名':'' })
+    this.setState({
+      usernameErrorMsg: !this.state.username?'请填写姓名':'',
+      usernameErrorColor: !this.state.username?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
   }
   handleDepartmentChange (event) {
     this.setState({
@@ -37,12 +45,15 @@ class CommentMain extends Component {
     });
     if(this.state.department) {
       this.setState({
-        departmentError: ''
+        departmentErrorMsg: ''
       })
     }
   }
   handleDepartmentBlur () {
-    this.setState({ departmentError: !this.state.department?'请填写所属部门':'' })
+    this.setState({
+      departmentErrorMsg: !this.state.department?'请填写所属部门':'',
+      departmentErrorColor: !this.state.department?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
   }
   handleDatesChange (event) {
     this.setState({
@@ -50,12 +61,15 @@ class CommentMain extends Component {
     })
     if(this.state.dates) {
       this.setState({
-        dateError: ''
+        dateErrorMsg: ''
       })
     }
   }
   handleDatesBlur () {
-    this.setState({ dateError: !this.state.dates?'请填写日期':'' })
+    this.setState({
+      dateErrorMsg: !this.state.dates?'请填写日期':'',
+      dateErrorColor: !this.state.dates?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
   }
   handleTimesChange (event) {
     this.setState({
@@ -63,12 +77,15 @@ class CommentMain extends Component {
     });
     if(this.state.times) {
       this.setState({
-        timeError: ''
+        timeErrorMsg: ''
       })
     }
   }
   handleTimesBlur () {
-    this.setState({ timeError: !this.state.times?'请填写时间段':'' })
+    this.setState({
+      timeErrorMsg: !this.state.times?'请填写时间段':'',
+      timeErrorColor: !this.state.times?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
   }
   handleWorkreasonChange (event) {
     this.setState({
@@ -76,12 +93,15 @@ class CommentMain extends Component {
     });
     if(this.state.workreason) {
       this.setState({
-        reasonError: ''
+        reasonErrorMsg: ''
       })
     }
   }
   handleWorkreasonBlur () {
-    this.setState({ reasonError: !this.state.workreason?'请填写加班事由':'' })
+    this.setState({
+      reasonErrorMsg: !this.state.workreason?'请填写加班事由':'',
+      reasonErrorColor: !this.state.workreason?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
   }
   handleRemarkChange (event) {
     this.setState({
@@ -89,11 +109,26 @@ class CommentMain extends Component {
     })
   }
   handleSubmit () {
-    this.setState({ usernameError: !this.state.username?'请填写姓名':'' })
-    this.setState({ departmentError: !this.state.department?'请填写所属部门':'' })
-    this.setState({ dateError: !this.state.dates?'请填写日期':'' })
-    this.setState({ timeError: !this.state.times?'请填写时间段':'' })
-    this.setState({ reasonError: !this.state.workreason?'请填写加班事由':'' })
+    this.setState({
+      usernameErrorMsg: !this.state.username?'请填写姓名':'',
+      usernameErrorColor: !this.state.username?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
+    this.setState({
+      departmentErrorMsg: !this.state.department?'请填写所属部门':'',
+      departmentErrorColor: !this.state.department?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
+    this.setState({
+      dateErrorMsg: !this.state.dates?'请填写日期':'',
+      dateErrorColor: !this.state.dates?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
+    this.setState({
+      timeErrorMsg: !this.state.times?'请填写时间段':'',
+      timeErrorColor: !this.state.times?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
+    this.setState({
+      reasonErrorMsg: !this.state.workreason?'请填写加班事由':'',
+      reasonErrorColor: !this.state.workreason?'comment-main-input comment-main-error-input':'comment-main-input'
+    })
   }
   render() {
     return (
@@ -101,13 +136,13 @@ class CommentMain extends Component {
         <div className='comment-main'>
           <div className='comment-main-name'>
             <h3>姓名<span className='font-col-up'> *</span></h3>
-            <input className={!this.state.reasonError?'comment-main-input':'comment-main-input comment-main-error-input'} valve={this.state.username} onChange={this.handleUsernameChange.bind(this)} onBlur={this.handleUsernameBlur.bind(this)} type='text' />
-            <div className='error-message font-col-up'>{this.state.usernameError}</div>
+            <input className={this.state.usernameErrorColor} valve={this.state.username} onChange={this.handleUsernameChange.bind(this)} onBlur={this.handleUsernameBlur.bind(this)} type='text' />
+            <div className='error-message font-col-up'>{this.state.usernameErrorMsg}</div>
           </div>
           <div className='comment-main-department'>
             <h3>所属部门<span className='font-col-up'> *</span></h3>
             <p>请正确选择自己所属的二级部门，不要都选择信息技术总部。</p>
-            <select className='comment-main-input' value={this.state.department} onChange={this.handleDepartmentChange.bind(this)} onBlur={this.handleDepartmentBlur.bind(this)}>
+            <select className={this.state.departmentErrorColor} value={this.state.department} onChange={this.handleDepartmentChange.bind(this)} onBlur={this.handleDepartmentBlur.bind(this)}>
               <option value=''>请选择</option>
               <option value='j3Hd'>信息技术总部</option>
               <option value='3y41'>技术管理部</option>
@@ -126,25 +161,25 @@ class CommentMain extends Component {
               <option value='cCTj'>信息安全部</option>
               <option value='kr4C'>数据研发服务部</option>
             </select>
-            <div className='error-message font-col-up'>{this.state.departmentError}</div>
+            <div className='error-message font-col-up'>{this.state.departmentErrorMsg}</div>
           </div>
           <div className='comment-main-date'>
             <h3>日期(请选择2017-11-21到2017-12-18之间的日期)<span className='font-col-up'> *</span></h3>
-            <input type='date' placeholder='年-月-日' min='2017-11-21' max='2017-12-18' value={this.state.dates} onChange={this.handleDatesChange.bind(this)} onBlur={this.handleDatesBlur.bind(this)} className='comment-main-input' />
-            <div className='error-message font-col-up'>{this.state.dateError}</div>
+            <input type='date' placeholder='年-月-日' min='2017-11-21' max='2017-12-18' value={this.state.dates} onChange={this.handleDatesChange.bind(this)} onBlur={this.handleDatesBlur.bind(this)} className={this.state.dateErrorColor} />
+            <div className='error-message font-col-up'>{this.state.dateErrorMsg}</div>
           </div>
           <div className='comment-main-time'>
             <h3>时间段<span className='font-col-up'> *</span></h3>
             <p>填写示例：8:30-17:30；</p>
             <p>默认时间段为8:30-17:30，如需修改请删掉默认值后重新填写</p>
-            <input className='comment-main-input' type='text' value={this.state.times} onChange={this.handleTimesChange.bind(this)} onBlur={this.handleTimesBlur.bind(this)} />
-            <div className='error-message font-col-up'>{this.state.timeError}</div>
+            <input className={this.state.timeErrorColor} type='text' value={this.state.times} onChange={this.handleTimesChange.bind(this)} onBlur={this.handleTimesBlur.bind(this)} />
+            <div className='error-message font-col-up'>{this.state.timeErrorMsg}</div>
           </div>
           <div className='comment-main-workreason'>
             <h3>加班事由<span className='font-col-up'> *</span></h3>
             <p>清算加班的统一写清算</p>
-            <input className='comment-main-input' type='text' value={this.state.workreason} onChange={this.handleWorkreasonChange.bind(this)} onBlur={this.handleWorkreasonBlur.bind(this)} />
-            <div className='error-message font-col-up'>{this.state.reasonError}</div>
+            <input className={this.state.reasonErrorColor} type='text' value={this.state.workreason} onChange={this.handleWorkreasonChange.bind(this)} onBlur={this.handleWorkreasonBlur.bind(this)} />
+            <div className='error-message font-col-up'>{this.state.reasonErrorMsg}</div>
           </div>
           <div className='comment-main-remark'>
             <h3>备注</h3>
